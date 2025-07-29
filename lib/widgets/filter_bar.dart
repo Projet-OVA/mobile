198729ef49e220d05bcd6eab719a6d1933eba2d0
oumy25 <kaneoumylaye@gmail.com>
2025-07-29 00:42:04 +0000

    ajout de actif tab

--- lib/widgets/filter_bar.dart
+++ lib/widgets/filter_bar.dart
@@ -1,48 +1,87 @@
 import 'package:flutter/material.dart';
 
-class FilterBar extends StatelessWidget {
+class FilterBar extends StatefulWidget {
   const FilterBar({super.key});
+
+  @override
+  State<FilterBar> createState() => _FilterBarState();
+}
+
+class _FilterBarState extends State<FilterBar> {
+  int selectedIndex = 0;
+
+  final List<Map<String, dynamic>> filters = [
+    {'icon': Icons.grid_view_outlined, 'label': 'Tout'},
+    {'icon': Icons.keyboard_voice_outlined, 'label': 'Podcast'},
+    {'icon': Icons.article, 'label': 'Articles'},
+    {'icon': Icons.headset_outlined, 'label': 'Audiobooks'},
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 20, bottom: 15),
+      padding: const EdgeInsets.only(left: 28, bottom: 15),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.start,
-        children: const [
-          FilterItem(icon: Icons.grid_view_outlined, label: 'Tout'),
-          SizedBox(width: 90),
-          FilterItem(icon: Icons.keyboard_voice_outlined, label: 'Podcast'),
-          SizedBox(width: 90),
-          FilterItem(icon: Icons.article, label: 'Articles'),
-          SizedBox(width: 90),
-          FilterItem(icon: Icons.headset_outlined, label: 'Audiobooks'),
-        ],
+        children: List.generate(filters.length, (index) {
+          final item = filters[index];
+          return Padding(
+            padding: const EdgeInsets.only(right: 25),
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedIndex = index;
+                });
+              },
+              child: FilterItem(
+                icon: item['icon'],
+                label: item['label'],
+                isActive: selectedIndex == index,
+              ),
+            ),
+          );
+        }),
       ),
     );
   }
 }
 
-
 class FilterItem extends StatelessWidget {
   final IconData icon;
   final String label;
+  final bool isActive;
 
   const FilterItem({
     super.key,
     required this.icon,
     required this.label,
+    this.isActive = false,
   });
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
+ @override
+Widget build(BuildContext context) {
+  return Container(
+    padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+    decoration: BoxDecoration(
+      color: isActive ? Color(0xFFFFF9E8) : Colors.transparent,
+      borderRadius: isActive ? BorderRadius.circular(25) : BorderRadius.circular(0),
+    ),
+    child: Row(
       mainAxisSize: MainAxisSize.min,
       children: [
-        Icon(icon, size: 24, color: Colors.black),
-        SizedBox(height: 4),
-        Text(label, style: TextStyle(fontSize: 12)),
+        Icon(
+          icon,
+          size: 14,
+        ),
+        const SizedBox(width: 4),
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 14,
+          ),
+        ),
       ],
-    );
-  }
+    ),
+  );
+}
 }
